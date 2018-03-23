d078f7914e07485ad3cc59c106de8318c7783b1a
ColdZhong <ColdZhong@gmail.com>
2018-03-23 11:44:26 +0800

    initialize the project at first time.

--- src/main/java/com/gd/orh/OnlineRideHailingApplication.java
+++ src/main/java/com/gd/orh/OnlineRideHailingApplication.java
@@ -2,11 +2,19 @@ package com.gd.orh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OnlineRideHailingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineRideHailingApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "hello world";
 	}
 }
