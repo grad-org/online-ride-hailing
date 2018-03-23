02eb010107aa90506650bcfde1546bdd45b4a20b
ColdZhong <ColdZhong@gmail.com>
2018-03-23 10:57:56 +0800

    initialize the project at first time.

---
+++ src/main/java/com/gd/orh/OnlineRideHailingApplication.java
@@ -0,0 +1,12 @@
+package com.gd.orh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRideHailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRideHailingApplication.class, args);
+	}
+}
