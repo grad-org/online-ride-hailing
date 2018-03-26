981bf6a4d0775d2187834aa20dc5a0e7b8fa28af
ColdZhong <ColdZhong@gmail.com>
2018-03-26 19:21:36 +0800

    initialize the project at first time.

--- src/main/java/com/gd/orh/OnlineRideHailingApplication.java
+++ src/main/java/com/gd/orh/OnlineRideHailingApplication.java
@@ -2,8 +2,12 @@ package com.gd.orh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
@@ -16,5 +20,18 @@ public class OnlineRideHailingApplication {
 	@RequestMapping("/")
 	public String index() {
 		return "hello world";
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("*");
+			}
+		};
 	}
 }
