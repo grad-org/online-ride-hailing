f1b20f4e705ee9b6377e6474647181bbbcdd8a59
Cold Zhong <ColdZhong@gmail.com>
2018-04-01 13:51:25 +0800

    修改了获取用户信息的方法

--- src/main/java/com/gd/orh/controller/LocationController.java
+++ src/main/java/com/gd/orh/controller/LocationController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
